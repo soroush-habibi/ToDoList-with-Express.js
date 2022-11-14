a2b769d8e0739ec279e3295d04b290800f32fa26
Soroush Habibi <soroush.habibi.1382@gmail.com>
2022-11-14 16:23:49 +0330

    use EJS template engine

--- index.js
+++ index.js
@@ -9,6 +9,9 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
 app.use("/", express.urlencoded({ extended: false }));
 app.use("/", express.json());
 app.use("/", express.static(path.join(__dirname, "public")));
